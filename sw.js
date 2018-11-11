9b024f3c2b2a62474ef3e92adc5bbeeb77a36fc6
Filiz Altinturk <info@filizaltinturk.com>
2018-11-11 17:28:37 +0300

    service worker

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE = "cache-and-update";
+var precacheFiles = [
+    "./css",
+    "./img",
+    "./font"
+];
+
+self.addEventListener("install", function (evt) {
+    console.log("The service worker is being installed.");
+    evt.waitUntil(precache());
+});
+
+self.addEventListener("fetch", function (evt) {
+    console.log("The service worker is serving the asset.");
+
+    evt.respondWith(fromCache(evt.request));
+
+    evt.waitUntil(update(evt.request));
+});
+
+function precache() {
+    return caches.open(CACHE).then(function (cache) {
+        return cache.addAll(precacheFiles);
+    });
+}
+
+function fromCache(request) {
+    return caches.open(CACHE).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            return matching || Promise.reject('no-match');
+        });
+    });
+}
+
+function update(request) {
+    return caches.open(CACHE).then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response);
+        });
+    });
+}
