f24bb9b87af4868714dfa642c98d2942575664a3
Filiz Altinturk <faltinturk@boynergrup.com>
2018-11-12 13:43:20 +0300

    🧐 precache

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-var CACHE = "cache-and-update";
+//This is the service worker with the Cache-first network
+
+var CACHE = 'pwabuilder-precache';
 var precacheFiles = [
     "/css/main.css",
     "/img/contact.png",
@@ -7,18 +9,28 @@ var precacheFiles = [
     "/font/nevis.woff2"
 ];
 
-self.addEventListener("install", function (evt) {
-    console.log("The service worker is being installed.");
-    evt.waitUntil(precache());
+//Install stage sets up the cache-array to configure pre-cache content
+self.addEventListener('install', function (evt) {
+    console.log('[PWA Builder] The service worker is being installed.');
+    evt.waitUntil(precache().then(function () {
+        console.log('[PWA Builder] Skip waiting on install');
+        return self.skipWaiting();
+    }));
 });
 
-self.addEventListener("fetch", function (evt) {
-    console.log("The service worker is serving the asset.");
 
-    evt.respondWith(fromCache(evt.request));
+//allow sw to control of current page
+self.addEventListener('activate', function (event) {
+    console.log('[PWA Builder] Claiming clients for current page');
+    return self.clients.claim();
+});
 
+self.addEventListener('fetch', function (evt) {
+    console.log('[PWA Builder] The service worker is serving the asset.' + evt.request.url);
+    evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
     evt.waitUntil(update(evt.request));
 });
+
 
 function precache() {
     return caches.open(CACHE).then(function (cache) {
@@ -27,6 +39,7 @@ function precache() {
 }
 
 function fromCache(request) {
+    //we pull files from the cache first thing so we can show them fast
     return caches.open(CACHE).then(function (cache) {
         return cache.match(request).then(function (matching) {
             return matching || Promise.reject('no-match');
@@ -35,9 +48,60 @@ function fromCache(request) {
 }
 
 function update(request) {
+    //this is where we call the server to get the newest version of the 
+    //file to use the next time we show view
     return caches.open(CACHE).then(function (cache) {
         return fetch(request).then(function (response) {
             return cache.put(request, response);
         });
     });
 }
+
+function fromServer(request) {
+    //this is the fallback if it is not in the cache to go to the server and get it
+    return fetch(request).then(function (response) { return response });
+}
+
+// var CACHE = "cache-and-update";
+// var precacheFiles = [
+//     "/css/main.css",
+//     "/img/contact.png",
+//     "/img/filiz-altinturk.png",
+//     "/img/whitewall.jpg",
+//     "/font/nevis.woff2"
+// ];
+// self.addEventListener("install", function (evt) {
+//     console.log("The service worker is being installed.");
+//     evt.waitUntil(precache());
+// });
+
+// self.addEventListener("fetch", function (evt) {
+//     console.log("The service worker is serving the asset.");
+
+//     evt.respondWith(fromCache(evt.request));
+
+//     evt.waitUntil(update(evt.request));
+// });
+
+// function precache() {
+//     return caches.open(CACHE).then(function (cache) {
+//         return cache.addAll(precacheFiles);
+//     });
+// }
+
+// function fromCache(request) {
+//     return caches.open(CACHE).then(function (cache) {
+//         return cache.match(request).then(function (matching) {
+//             return matching || Promise.reject('no-match');
+//         });
+//     });
+// }
+
+// function update(request) {
+//     return caches.open(CACHE).then(function (cache) {
+//         return fetch(request).then(function (response) {
+//             return cache.put(request, response);
+//         });
+//     });
+// }
+
